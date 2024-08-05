9e8217535efd85f49dc315742a62889536c1a587
KillerA-z1 <Ahmedz1a2m@gmail.com>
2024-08-06 01:43:53 +0300

    Hot Fix 02

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-from flask import Flask
+"""The controller for the api"""
+
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 import os
 
 app = Flask(__name__)
+
 app.register_blueprint(app_views)
 
 
@@ -23,6 +26,6 @@ def teardown_db(exception):
 
 
 if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.getenv("HBNB_API_PORT", 5000))
-    app.run(host=host, port=port, threaded=True)
+    HBNB_API_HOST = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    HBNB_API_PORT = int(os.getenv("HBNB_API_PORT", 5000))
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
